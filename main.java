3d7cf63c5910a48734b2e07350ef2522451c41f9
Sandy Jones <sandynashv@gmail.com>
2017-04-10 12:51:48 -0500

    beginning of collection of java code

---
+++ main.java
@@ -0,0 +1,16 @@
+public class codeSample {
+	public static void main(String[] args){
+		int points = 21*3;
+		System.out.println(points);
+	}
+	public static void main(String[] args){
+		int x = 3;
+		for(i = 0; i <= 6; i ++){
+			System.out.prinln(x);
+		}	
+	}
+}
+
+
+
+
